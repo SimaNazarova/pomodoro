e61cec1667d66edd01d76f2b3b6f99ee6dca0186
Serafima Nazarova <sima.nazarova@gmail.com>
2022-04-01 17:04:52 +0400

    do markup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>title</h2>
+      <div className="timer">
+        <span>minutes</span>
+        <span>:</span>
+        <span>seconds</span>
+      </div>
+      <div className="btns">
+        <div>Start</div>
+        <div>Stop</div>
+        <div>Reset</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
