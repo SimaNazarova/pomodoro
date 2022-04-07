0345c728fce41a7f54f271bbe276c5967b8ae1cf
Serafima Nazarova <sima.nazarova@gmail.com>
2022-04-07 11:42:05 +0400

    add functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
+import React, { useState, useRef } from "react";
+import { padTime } from "./utils/utils";
 import "./App.css";
 
 function App() {
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [subtitle, setSubtitle] = useState("Let's do it");
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+
+  const startTimer = () => {
+    setIsRunning(true);
+    if (intervalRef.current !== null) return;
+    setSubtitle("You're doing great!");
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft >= 1) return timeLeft - 1;
+        resetTimer();
+        return 0;
+      });
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    if (intervalRef.current === null) return;
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setSubtitle("Don't give up!");
+    setIsRunning(false);
+  };
+
+  const resetTimer = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setSubtitle("Ready to go another round?");
+    setTimeLeft(25 * 60);
+    setIsRunning(false);
+  };
   return (
     <div className="App">
       <h2 className="title">Pomodoro</h2>
-      <h3 className="subtitle">let's do it</h3>
+      <h3 className="subtitle">{subtitle}</h3>
       <div className="timer">
-        <span>30</span>
-        <span>:</span>
-        <span>40</span>
+        <div className="timer__container">
+          <span>{minutes}</span>
+          <span>:</span>
+          <span>{seconds}</span>
+        </div>
       </div>
       <div className="btns">
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
   );
---
+++ src/utils/utils.js
@@ -0,0 +1,4 @@
+export const padTime = (time) => {
+    return time.toString().padStart(2, "0");
+  };
+  
